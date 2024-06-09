f6cd4f074933c0d620dec50dec22d8d23edb3d15
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-06-09 16:00:25 +0300

    feat: Add Favourites page and update routing

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -8,13 +8,16 @@ import NotFound from "../../pages/NotFound";
 import Home from "../../pages/Home";
 import Login from "../../features/auth/Login";
 import Signup from "../../features/auth/Signup";
+import Favourites from "../../features/favourites/Favourites";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Home />} />
+      {/* <Route path="/" element={<Home />} errorElement={<ErrorPage />} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/favourites" element={<Favourites />} />
       <Route path="*" element={<NotFound />} />
     </>
   )
---
+++ src/features/favourites/Favourites.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function Favourites() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        My Favorites
+      </Typography>
+    </div>
+  );
+}
