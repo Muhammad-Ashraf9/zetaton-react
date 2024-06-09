2a88524ecf8e465df03a35a8ae5f5039bde95b43
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-06-09 10:42:51 +0300

    chore: Update Navbar component
    Refactor the Navbar component to use the latest Material-UI syntax and styling conventions.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,44 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 import { NavLink } from "react-router-dom";
+
+const style = {
+  color: "white",
+  textDecoration: "none",
+};
 
 function Navbar() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">Zetaton</Typography>
-      </Toolbar>
-
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/signup">Signup</NavLink>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="error" elevation={0}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Zetaton
+          </Typography>
+          <Button color="inherit">
+            <NavLink to="/login" style={style}>
+              Login
+            </NavLink>
+          </Button>
+          <Button color="inherit">
+            <NavLink to="/signup" style={style}>
+              Signup
+            </NavLink>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
