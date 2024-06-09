874f22fd89f329759334ad1b04ccc8e70f667cd9
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-06-09 21:40:29 +0300

    refactor: Remove useDispatch from Logout component

--- src/features/auth/Logout.jsx
+++ src/features/auth/Logout.jsx
@@ -1,14 +1,10 @@
 import { Button } from "@mui/material";
-import { useDispatch } from "react-redux";
 import { logout } from "./authService";
-import { clearUser } from "./authSlice";
 
 function Logout() {
-  const dispatch = useDispatch();
   async function handleLogout() {
     try {
       await logout();
-      dispatch(clearUser());
     } catch (error) {
       console.error(error);
     }
