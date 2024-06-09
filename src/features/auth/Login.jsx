3898634b9e6cd6c28ba2fef18ad2e87b876322fa
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-06-09 10:26:06 +0300

    chore: Update login component
    The code changes include:
    - Importing `authErrorMessages` from `../../config/firebaseErrors` to handle error messages from Firebase authentication.
    - Handling Firebase authentication errors and displaying them in the login form.

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -12,9 +12,9 @@ import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
 import { auth } from "../../config/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import authErrorMessages from "../../config/firebaseErrors";
 
 function Login() {
-
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [error, setError] = useState(null);
 
@@ -36,72 +36,68 @@ function Login() {
       console.log("user :>> ", user);
       console.log("auth.currentUser :>> ", auth.currentUser);
     } catch (error) {
-      setError(error.message);
+      console.error("error :>> ", error);
+      setError(authErrorMessages[error.code]);
     }
   };
 
   return (
-      <Container component="main" maxWidth="xs" className="login-container">
-        <CssBaseline />
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 2, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+    <Container component="main" maxWidth="xs" className="login-container">
+      <CssBaseline />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 2, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Log in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={handleChange}
+            value={formData.email}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={handleChange}
+            value={formData.password}
+          />
+          {error && (
+            <Stack sx={{ width: "100%" }} spacing={2}>
+              <Alert severity="error">{error}</Alert>
+            </Stack>
+          )}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={handleChange}
-              value={formData.email}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={handleChange}
-              value={formData.password}
-            />
-            {error && (
-              <Stack sx={{ width: "100%" }} spacing={2}>
-                <Alert severity="error">{error}</Alert>
-              </Stack>
-            )}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Log in
-            </Button>
-          </Box>
+            Log in
+          </Button>
         </Box>
-      </Container>
+      </Box>
+    </Container>
   );
 }
 
